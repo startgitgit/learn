b10a63db7238b8f83d6ea9c73693642710f76296
zhouyq <309439818@qq.com>
2017-10-25 23:07:08 +0800

    regex

---
+++ code/java/practice/src/main/java/regex/program.java
@@ -0,0 +1,9 @@
+package regex;
+
+public class program {
+    public static void main(String[] args) {
+        String a ="\\";
+        System.out.println(a);
+        System.out.println(a.replaceAll("\\\\\\{",""));
+    }
+}
