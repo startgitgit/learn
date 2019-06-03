d04637deea56d236c60768ab37eaf266c1f66fbe
zhouyq <309439818@qq.com>
2019-06-03 21:59:44 +0800

    lambda

--- code/java/practice/base/src/main/java/stream/Program.java
+++ code/java/practice/base/src/main/java/stream/Program.java
@@ -1,7 +1,7 @@
 package stream;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @Author: zhouyq
@@ -13,5 +13,9 @@ public class Program {
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
         long count = integers.stream().filter(x -> x == 4).count();
+        List<Integer> result = Optional.ofNullable(integers).
+                orElse(Collections.emptyList()).stream().filter(Objects::nonNull).
+                map(x -> x * 2).collect(Collectors.toList());
+        System.out.println(result);
     }
 }
