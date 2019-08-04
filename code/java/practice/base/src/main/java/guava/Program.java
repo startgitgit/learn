2495467462e6a8ed8258e23241fa115dc57be908
zhouyq <309439818@qq.com>
2019-08-04 20:50:24 +0800

    guava cache

--- code/java/practice/base/src/main/java/guava/Program.java
+++ code/java/practice/base/src/main/java/guava/Program.java
@@ -3,6 +3,13 @@ package guava;
 import com.google.common.base.CharMatcher;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.cache.*;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: zhouyq
@@ -28,6 +35,50 @@ public class Program {
 
         int i = CharMatcher.anyOf("ab").indexIn("123ab");
         System.out.println(i);
+
+        System.out.println(Strings.emptyToNull(" ") == null);
+
+        Multimap<Integer, String> keyValues = ArrayListMultimap.create();
+        keyValues.put(1, "a");
+        keyValues.put(1, "b");
+        keyValues.put(2, "c");
+        System.out.println(keyValues.toString());
+        System.out.println(keyValues.get(1));
+
+        LoadingCache<String, String> cache = CacheBuilder.newBuilder()
+                //设置大小，条目数
+                .maximumSize(20)
+                //设置失效时间，创建时间
+                //.expireAfterWrite(10, TimeUnit.SECONDS)
+                //设置时效时间，最后一次被访问
+                .expireAfterAccess(10, TimeUnit.SECONDS)
+                //移除缓存的监听器
+                .removalListener(new RemovalListener<String, String>() {
+                    public void onRemoval(RemovalNotification<String, String> notification) {
+                        System.out.println("有缓存数据被移除了");
+                    }
+                })
+                //缓存构建的回调
+                .build(new CacheLoader<String, String>() {//加载缓存
+                    @Override
+                    public String load(String key) throws Exception {
+                        return key + "-" + "world";
+                    }
+                });
+
+        try {
+            System.out.println(cache.get("hello"));
+//            cache.invalidateAll();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Thread.sleep(30000);
+            System.out.println(cache.get("hello"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
