1e244645a5d801c171a6bdfd51db36b9c488eeb5
zhouyq <309439818@qq.com>
2019-08-15 22:12:15 +0800

    数组转stream或空值处理

--- code/java/practice/base/src/main/java/stream/Program.java
+++ code/java/practice/base/src/main/java/stream/Program.java
@@ -1,5 +1,7 @@
 package stream;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -11,11 +13,20 @@ import java.util.stream.Collectors;
  */
 public class Program {
     public static void main(String[] args) {
+
+        //List转stream
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
         long count = integers.stream().filter(x -> x == 4).count();
+
         List<Integer> result = Optional.ofNullable(integers).
                 orElse(Collections.emptyList()).stream().filter(Objects::nonNull).
                 map(x -> x * 2).collect(Collectors.toList());
         System.out.println(result);
+
+
+        //数组转stream
+        Arrays.stream(Optional.ofNullable(args).orElse(new String[0])).forEach(System.out::println);
+
+
     }
 }
