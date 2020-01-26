cc0ed6a1e743d16e6499d801f67b1f823001db09
zhouyq <309439818@qq.com>
2020-01-26 09:44:59 +0800

    add bcrypt

---
+++ code/java/practice/base/src/main/java/bcryptencode/Program.java
@@ -0,0 +1,46 @@
+package bcryptencode;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * @Author: zhouyq
+ * @Date: 2020/1/26 9:40
+ * @Version 1.0
+ * @Description
+ */
+public class Program {
+    public static void main(String[] args) {
+        String password = "12345";
+        String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println(hashed);
+
+        String hashed01 = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println(hashed01);
+
+        String hashed02 = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println(hashed02);
+
+
+        String hashed03 = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println(hashed03);
+
+
+        String hashed04 = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println(hashed04);
+
+        String hashed05 = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println(hashed05);
+// gensalt's log_rounds parameter determines the complexity
+// the work factor is 2**log_rounds, and the default is 10
+        String hashed2 = BCrypt.hashpw(password, BCrypt.gensalt(12));
+
+// Check that an unencrypted password matches one that has
+// previously been hashed
+        String candidate = "12345";
+
+        if (BCrypt.checkpw(candidate, hashed))
+            System.out.println("It matches");
+        else
+            System.out.println("It does not match");
+    }
+}
