802e164c9f8977a32e4079d3382f4d2286f9a908
zhouyanqing <309439818@qq.com>
2020-03-25 19:08:18 +0800

    优化

--- code/java/practice/base/src/main/java/math/Test.java
+++ code/java/practice/base/src/main/java/math/Test.java
@@ -2,7 +2,7 @@ package math;
 
 public class Test {
     public static void main(String[] args) {
-        Double a = 30.0 / 30;
+        double a = 30.0 / 30;
         System.out.println(Math.round(a * 1000) / 1000.0);
         int round = Math.round(3.145f);
     }
