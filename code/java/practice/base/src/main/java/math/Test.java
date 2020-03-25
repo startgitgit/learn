80d0f3c3bdd3773eb23d864f816e2a022a5a9f2c
zhouyanqing <309439818@qq.com>
2020-03-25 20:05:30 +0800

    优化

--- code/java/practice/base/src/main/java/math/Test.java
+++ code/java/practice/base/src/main/java/math/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         double a = 30.0 / 30;
         System.out.println(Math.round(a * 1000) / 1000.0);
-        int round = Math.round(3.145f);
+        int round = Math.round(3.545f);
+        System.out.println(round);
     }
 }
