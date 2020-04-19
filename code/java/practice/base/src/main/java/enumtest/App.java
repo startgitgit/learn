168a150f965e28e2ccece7b49891d7301a0051e6
zhouyanqing <309439818@qq.com>
2020-04-19 10:00:48 +0800

    enum

--- code/java/practice/base/src/main/java/enumtest/App.java
+++ code/java/practice/base/src/main/java/enumtest/App.java
@@ -2,6 +2,9 @@ package enumtest;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(Enum.valueOf(Day.class,"MONDAY1"));
+        System.out.println(Enum.valueOf(Day.class,"MONDAY"));
+        System.out.println(Color.valueOf(Color.class,"RED"));
+
+
     }
 }
