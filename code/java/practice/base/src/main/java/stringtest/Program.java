4f75367de5a2160335b630e48d6892e025af0219
zhouyanqing <309439818@qq.com>
2020-05-17 00:19:11 +0800

    string test

--- code/java/practice/base/src/main/java/stringtest/Program.java
+++ code/java/practice/base/src/main/java/stringtest/Program.java
@@ -1,5 +1,8 @@
 package stringtest;
 
+
+import org.apache.directory.api.util.Strings;
+
 /**
  * @author zhouyq
  * @version 1.0
@@ -7,15 +10,18 @@ package stringtest;
  */
 public class Program {
     public static void main(String[] args) {
-        String str = "abcda\\ab";
+        String str = "a4bc\\";
+        System.out.println(str);
         System.out.println(str.length());
-        System.out.println(str.charAt(str.length()-1));
+        System.out.println(str.charAt(str.length() - 1));
         System.out.println(str.substring(0));
-        System.out.println(str.substring(0,str.length()-1));
+        System.out.println(str.substring(0, str.length() - 1));
         char c = 'a';
-        System.out.println(str.indexOf(c));
-        System.out.println(str.replace('a','x'));
-        System.out.println(str.replaceFirst("a$","7"));
-        System.out.println(str.replaceAll("\\\\$","\\\\\\\\"));
+        System.out.println("index:" + str.indexOf(c));
+        System.out.println(str.replace('a', 'x'));
+        System.out.println(str.replaceFirst("a$", "7"));
+        // replaceAll 的第一个参数是正则表达式，故而要经过两次转义，一次Java、一次正则。因此就需要四个反斜杠才可以匹配一个反斜杠。
+        System.out.println(str.replaceAll("\\\\$", "\\\\\\\\"));
+        System.out.println(str.replaceAll("\\d", "-"));
     }
 }
