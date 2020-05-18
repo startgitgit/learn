1ccdcf8db82b3c2ecede8eced7a09eaa311cea29
zhouyanqing <309439818@qq.com>
2020-05-18 23:11:14 +0800

    jdbc connection threadLocal

--- code/java/practice/base/src/main/java/jdbc/Program.java
+++ code/java/practice/base/src/main/java/jdbc/Program.java
@@ -1,13 +1,20 @@
 package jdbc;
 
 import com.google.common.base.Preconditions;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.apache.commons.lang3.StringUtils;
+import org.postgresql.ds.PGPoolingDataSource;
+import thread.ThreadService;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 /**
  * @author zhouyq
@@ -19,27 +26,46 @@ public class Program {
         String url = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai";
         String username = "root";
         String password = "zaq12345";
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setUser(username);
+        mysqlDataSource.setPassword(password);
+        mysqlDataSource.setUrl(url);
+        PGPoolingDataSource pgPoolingDataSource = new PGPoolingDataSource();
+        Connection connection1 = pgPoolingDataSource.getConnection();
+        connection1.close();
+
+
         ThreadLocal<Connection> connectionThreadLocal = ThreadLocal.withInitial(() -> {
             try {
-                return DriverManager.getConnection(url, username, password);
-            } catch (SQLException e) {
+//                return DriverManager.getConnection(url, username, password);
+
+                return  mysqlDataSource.getConnection();
+            } catch (Exception e) {
                 e.printStackTrace();
                 return null;
             }
         });
-        CompletableFuture.runAsync(() -> {
 
-            Connection connection = connectionThreadLocal.get();
+        Arrays.asList(1,2,3,4,5,6,7,8,9).parallelStream().forEach(x -> {
+//            Connection connection = connectionThreadLocal.get();
+            Connection connection = null;
+            try {
+                connection = mysqlDataSource.getConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return;
+            }
             PreparedStatement preparedStatement = null;
             Preconditions.checkNotNull(connection);
             try {
+//                Thread.sleep(10000);
                 connection.setAutoCommit(false);
-                String sql = "INSERT INTO `test`.`employee`(`id`, `name`, `gender`, `birthday`, `idCard`, `wedlock`, `nationId`, `nativePlace`, `politicId`, `email`, `phone`, `address`, `departmentId`, `jobLevelId`, `posId`, `engageForm`, `tiptopDegree`, `specialty`, `school`, `beginDate`, `workState`, `workID`, `contractTerm`, `conversionTime`, `notWorkDate`, `beginContract`, `endContract`, `workAge`) VALUES (101002, '江南一点雨', '男', '1990-01-01', '610122199001011256', '已婚', 1, '陕西', 13, 'laowang@qq.com', '18565558897', '深圳市南山区', 5, 9, 29, '劳务合同', '本科', '信息管理与信息系统', '深圳大学', '2018-01-01', '在职', '00000001', 2, '2018-04-01', NULL, '2018-01-01', '2020-01-01', NULL);\n";
+                String sql = "INSERT INTO `test`.`employee`(`id`) VALUES (" + x +200000+");";
                 preparedStatement = connection.prepareStatement(sql);
                 Preconditions.checkNotNull(preparedStatement);
                 preparedStatement.executeUpdate();
 
-                String sql2 = "INSERT INTO `test`.`employee`(`id`, `name`, `gender`, `birthday`, `idCard`, `wedlock`, `nationId`, `nativePlace`, `politicId`, `email`, `phone`, `address`, `departmentId`, `jobLevelId`, `posId`, `engageForm`, `tiptopDegree`, `specialty`, `school`, `beginDate`, `workState`, `workID`, `contractTerm`, `conversionTime`, `notWorkDate`, `beginContract`, `endContract`, `workAge`) VALUES (101000, '江南一点雨', '男', '1990-01-01', '610122199001011256', '已婚', 1, '陕西', 13, 'laowang@qq.com', '18565558897', '深圳市南山区', 5, 9, 29, '劳务合同', '本科', '信息管理与信息系统', '深圳大学', '2018-01-01', '在职', '00000001', 2, '2018-04-01', NULL, '2018-01-01', '2020-01-01', NULL);\n";
+                String sql2 =  "INSERT INTO `test`.`employee`(`id`) VALUES (" + x +500000+");";
                 PreparedStatement preparedStatement2 = connection.prepareStatement(sql2);
                 preparedStatement2.executeUpdate();
                 connection.commit();
@@ -54,10 +80,45 @@ public class Program {
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-
             }
+        });
 
-        }).get();
+
+        /*for (int i = 0; i < 5; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                Connection connection = connectionThreadLocal.get();
+                PreparedStatement preparedStatement = null;
+                Preconditions.checkNotNull(connection);
+                try {
+                    connection.setAutoCommit(false);
+                    String sql = "INSERT INTO `test`.`employee`(`id`) VALUES (" + finalI +200000+");";
+                    preparedStatement = connection.prepareStatement(sql);
+                    Preconditions.checkNotNull(preparedStatement);
+                    preparedStatement.executeUpdate();
+
+                    String sql2 =  "INSERT INTO `test`.`employee`(`id`) VALUES (" + finalI +500000+");";
+                    PreparedStatement preparedStatement2 = connection.prepareStatement(sql2);
+                    preparedStatement2.executeUpdate();
+                    connection.commit();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        connection.rollback();
+                        assert preparedStatement != null;
+                        preparedStatement.close();
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }*/
+
+
+
+        System.out.println(StringUtils.center("end",50,"="));
 
 
     }
