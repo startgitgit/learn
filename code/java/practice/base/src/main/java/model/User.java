3fe1364057d75280d0a9815f8953c596d85b50f7
zhouyanqing <309439818@qq.com>
2020-06-09 21:58:47 +0800

    easymock

---
+++ code/java/practice/base/src/main/java/model/User.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * @author zhouyq
+ * @version 1.0
+ * @date 2020/6/9 21:33
+ */
+public class User extends Person {
+    public User(String name, int age) {
+        super(name, age);
+    }
+}
