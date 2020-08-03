4ae9412ca41e8092796a6ea5276419f97b150224
zhouyanqing <309439818@qq.com>
2020-08-03 20:40:01 +0800

    case

--- code/scala/practice/src/main/scala/casetest/App.scala
+++ code/scala/practice/src/main/scala/casetest/App.scala
@@ -8,11 +8,31 @@ package casetest
 object App {
   def main(args: Array[String]): Unit = {
     val arr = Array(1, 2, 3, 4, 5, 6)
+
+    //模式匹配
     arr.foreach {
-      case 1 => println(1)
-      case 2 => println(2)
-      case _ => println("Other")
+      case 1 => println("==1")
+      case _ => println("!=1")
     }
 
+    arr.foreach(x => {println(x)})
+
+    // case class
+    val message = Message("xxx", "ttt")
+    println(message)
+
+   //偏函数
+    val p1:PartialFunction[String, String] = {
+      case s if s.contains("1") => s.replace("1", "one")
+    }
+    val p2:PartialFunction[String, String] = {
+      case s if s.contains("2") => s.replace("2", "two")
+    }
+    val p = p1 andThen p2
+    p("12")
+
   }
+
+  case class Message(sender: String, messageContent: String)
+
 }
