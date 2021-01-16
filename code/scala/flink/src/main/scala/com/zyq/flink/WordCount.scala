0d01ac5668ec121ae44ffa5e1b46d181cb76a1f0
zhouyanqing <309439818@qq.com>
2021-01-16 22:29:57 +0800

    flink update version 1.12

--- code/scala/flink/src/main/scala/com/zyq/flink/WordCount.scala
+++ code/scala/flink/src/main/scala/com/zyq/flink/WordCount.scala
@@ -1,18 +1,23 @@
 package com.zyq.flink
 
 // 导入一些隐式类型转换，implicit
+
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 
 object WordCount {
   def main(args: Array[String]): Unit = {
     // 获取运行时环境，类似sparkcontext
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // 老版本
+    //  env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
     // 设置分区(并行任务)的数量为1
-//    env.setParallelism(1)
+    //    env.setParallelism(1)
     // 获取建立数据源
     // 需要先启动nc -lk 9999,用来发送数据
-    val stream = env.socketTextStream("localhost", 9999, '\n')
+    val stream: DataStream[String] = env.socketTextStream("localhost", 9999, '\n')
     // 写对流的转换处理逻辑
     val transformed: DataStream[WordWithCount] = stream
       // 使用空格切分输入的字符串
@@ -20,9 +25,13 @@ object WordCount {
       // 类似与mr的map
       .map(w => WordWithCount(w, 1))
       // 使用word字段进行分组，shuffle
-      .keyBy(0)
+      .keyBy(x => x.word)
+      //老版本
+      //      .keyBy(0)
       //开了一个5s钟的滚动窗口
-      .timeWindow(Time.seconds(5))
+      .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
+      // 老版本
+      //.timeWindow(Time.seconds(5))
       // 针对count字段进行累加操作，类型mr的reduce
       .sum(1)
 
