3884408f90bf6b1b3fad16e81d20a8525d4d350d
zhouyanqing <309439818@qq.com>
2021-01-29 07:33:53 +0800

    leetcode

---
+++ code/java/practice/base/src/main/java/algorithm/Test.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+/**
+ * @author zhouyq
+ * @version 1.0
+ * @date 2021/1/28 21:59
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1 ^ 1);
+    }
+}
