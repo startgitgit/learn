321f2f41bd88b20d5f7216a2b1b77cbbec48ed83
zhouyanqing <309439818@qq.com>
2021-03-01 00:04:22 +0800

    中位数

--- code/java/practice/base/src/main/java/algorithm/SubSets.java
+++ code/java/practice/base/src/main/java/algorithm/SubSets.java
@@ -1,6 +1,34 @@
-package algorithm;/**
- * @author  zhouyq
- * @date  2021/1/31 7:53
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhouyq
  * @version 1.0
- */public class SubSets {
+ * @date 2021/1/31 7:53
+ */
+public class SubSets {
+    List<Integer> t = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            for (int i = 0; i < n; ++i) {
+                if ((mask & (1 << i)) != 0) {
+                    t.add(nums[i]);
+                }
+            }
+            ans.add(new ArrayList<Integer>(t));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        SubSets sets = new SubSets();
+        sets.subsets(new int[]{1,2,3});
+    }
+
 }
--- code/java/practice/base/src/main/java/algorithm/SubSets1.java
+++ code/java/practice/base/src/main/java/algorithm/SubSets1.java
@@ -1,6 +1,35 @@
-package algorithm;/**
- * @author  zhouyq
- * @date  2021/1/31 8:16
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhouyq
  * @version 1.0
- */public class SubSets1 {
+ * @date 2021/1/31 8:16
+ */
+public class SubSets1 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        int pow = (int) Math.pow(2, n);
+        int pow1 = (int) Math.pow(2, n + 1);
+        for(int i = pow; i< pow1; i++){
+            String bitmask=Integer.toBinaryString(i).substring(1);
+            List<Integer> cur= new ArrayList<>();
+            for (int j =0;j<n;j++){
+                if(bitmask.charAt(j) == '1') {
+                    cur.add(nums[j]);
+                }
+            }
+            result.add(cur);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SubSets1 subSets1 = new SubSets1();
+        subSets1.subsets(new int[] {1,2,3});
+    }
 }
+
